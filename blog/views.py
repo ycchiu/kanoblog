bc62dc5d3a06cbaab5d74aaefd742514cde94546
Ya-ching Chiu <tracy720329@gmail.com>
2015-02-07 16:40:46 -0800

    adding new form to post_new function

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from .forms import PostForm
 from .models import Post
+
 
 def post_list(request):
 	posts = Post.objects.filter(published_date__isnull=False).order_by('published_date')
@@ -8,3 +10,15 @@ def post_list(request):
 def post_detail(request, pk):
 	post = get_object_or_404(Post, pk=pk)
 	return render(request, 'blog/post_detail.html', {'post':post})
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect('blog.views.post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_new.html', {'form': form})
